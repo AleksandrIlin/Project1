c2e00e59290f81fc38f7c56e7bf240b3224cc52f
Ilin Aleksandr <aqwa06@gmail.com>
2024-06-07 21:02:40 +0500

    Moved main.py to the main directory, created a logs folder, set up logging of the masks and utils files

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,43 @@
+import logging
+
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/masks.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_cards(number: str) -> str:
     """Функция возвращает маску карты"""
+    logger.info(f'Проверяем number что он > 0')
     if len(number) > 0:
+        logger.info(f'Проверяем сто number равен 16 символам')
         if len(number) == 16:
+            logger.info(f'Маскируем number согласно условия задачи')
             result = f"{number[0:4]} {number[4:6]}** **** {number[12:]}"
+            logger.info(f'Выводим результат маскировки')
             return result
         else:
+            logger.info(f'Выводим результат если number не равен 16 символам')
             return ""
+    logger.info(f'Выводим результат если number меньше или равен нулю')
     return ""
 
 
 def get_mask_account(number: str) -> str:
     """Функция возвращает маску счёта"""
+    logger.info(f'Проверяем number что он > 0')
     if len(number) > 0:
+        logger.info(f'Проверяем сто number равен 20 символам')
         if len(number) == 20:
+            logger.info(f'Маскируем number согласно условия задачи')
             result = f"**{number[-4:]}"
             return result
         else:
+            logger.info(f'Выводим результат если number не равен 20 символам')
             return ""
+    logger.info(f'Выводим результат если number меньше или равен нулю')
     return ""
+
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
 import json
+import logging
+
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_transactions(file_path: str) -> list[dict]:
@@ -7,6 +16,7 @@ def get_transactions(file_path: str) -> list[dict]:
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
     try:
+        logger.info(f'открываем json файл {file_path}')
         with open(file_path, "r", encoding="utf-8") as file:
             repos = json.load(file)
             if isinstance(repos, list):
@@ -14,4 +24,5 @@ def get_transactions(file_path: str) -> list[dict]:
             else:
                 return []
     except (FileNotFoundError, json.JSONDecodeError):
+        logger.error('Error message')
         return []
